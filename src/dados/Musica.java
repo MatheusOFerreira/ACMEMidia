1e29e22f6e6fed2820b963465079bd04aa2d0d6f
Matheus de Oliveira Ferreira <matheus.oferreira@hotmail.com>
2024-05-28 16:37:55 -0300

    Implementacao das classes Video e Musica

--- src/dados/Musica.java
+++ src/dados/Musica.java
@@ -1,5 +1,36 @@
 package dados;
 
-public class Musica {
+public class Musica extends Midia {
+
+	private double duracao;
+
+	public Musica(int codigo, String titulo, int ano, Categoria categoria, double duracao) {
+		super(codigo, titulo, ano, categoria);
+		this.duracao = duracao;
+	}
+
+	@Override
+	public double calculaLocacao() {
+		double valorPorMinuto = 0.0;
+
+		switch (getCategoria()) {
+		case ACA:
+			valorPorMinuto = 1.0;
+			break;
+		case DRA:
+			valorPorMinuto = 0.8;
+			break;
+		case FIC:
+			valorPorMinuto = 1.2;
+			break;
+		case ROM:
+			valorPorMinuto = 0.9;
+			break;
+		default:
+			throw new IllegalArgumentException("Categoria desconhecida: " + getCategoria());
+		}
+
+		return valorPorMinuto * duracao;
+	}
 
 }
--- src/dados/Video.java
+++ src/dados/Video.java
@@ -1,5 +1,24 @@
 package dados;
 
-public class Video {
+public class Video extends Midia {
+
+	private int qualidade;
+
+	public Video(int codigo, String titulo, int ano, Categoria categoria, int qualidade) {
+		super(codigo, titulo, ano, categoria);
+		this.qualidade = qualidade;
+	}
+
+	@Override
+	public double calculaLocacao() {
+		double valorLocacao = 0.0;
+		if (super.getAno() == 2024) {
+			valorLocacao = 20.00;
+		} else if (super.getAno() >= 2000 && super.getAno() <= 2023) {
+			valorLocacao = 15.00;
+		} else
+			valorLocacao = 10.00;
+		return valorLocacao;
+	}
 
 }
